b7f69c0de9a7531eeecf84d5ed98d4abbf7a527a
Diego Camino Reinoso <dcamino@fruitful.co>
2022-10-08 20:59:34 -0500

    Integrate web3auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import Home from "./containers/Home";
 import LandingPage from "./containers/LandingPage";
 import { Route, Routes } from "react-router-dom";
 import PageNotFound from "./containers/PageNotFound";
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/test" element={<LandingPage />} />
         <Route path="/test" element={<LandingPage />} />
         <Route path="*" element={<PageNotFound />} />
